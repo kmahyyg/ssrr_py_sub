34676d32a127e1e63ea6ec25d3ca8d02fe00a528
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-04-17 23:21:26 +0800

    chore(runclient.sh): add client ran notification

--- runclient.sh
+++ runclient.sh
@@ -2,4 +2,5 @@
 python3 ./main.py
 cp ./clientconf.json ./shadowsocksr/shadowsocks/
 nohup python3 ./shadowsocksr/shadowsocks/local.py -c ./shadowsocksr/shadowsocks/clientconf.json > /dev/null 2>&1 &
+echo "Client successfully ran in the background!"
 exit 0
