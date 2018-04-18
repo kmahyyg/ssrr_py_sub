c151e0c5f2e1d6cc42e62df9f4564b85a51e916c
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-04-18 20:05:17 +0800

    fix(chdir to cwd error):

--- runclient.sh
+++ runclient.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd $(dirname $(readlink -f $0))
 python3 ./main.py
 cp ./clientconf.json ./shadowsocksr/shadowsocks/
 nohup python3 ./shadowsocksr/shadowsocks/local.py -c ./shadowsocksr/shadowsocks/clientconf.json > /dev/null 2>&1 &
