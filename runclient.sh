203a9f18516dfe2fe6458d6393e3731d0c684c5b
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-04-19 12:23:28 +0800

    fix(runclient.sh): client permission error

--- runclient.sh
+++ runclient.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $(dirname $(readlink -f $0))
-python3 ./main.py
-cp ./clientconf.json ./shadowsocksr/shadowsocks/
+sudo python3 ./main.py
+sudo cp ./clientconf.json ./shadowsocksr/shadowsocks/
 nohup python3 ./shadowsocksr/shadowsocks/local.py -c ./shadowsocksr/shadowsocks/clientconf.json > /dev/null 2>&1 &
 echo "Client successfully ran in the background!"
 exit 0
