43b27adf579adcf4dc0af6426a0fcfe0f6a0b684
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-04-30 20:00:42 +0800

    fix(runclient.sh): restore original shell script

--- runclient.sh
+++ runclient.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-usernow=$(whoami)
 cd $(dirname $(readlink -f $0))
 sudo python3 ./main.py
 sudo cp ./clientconf.json ./shadowsocksr/shadowsocks/
-python3 ./shadowsocksr/shadowsocks/local.py -c ./shadowsocksr/shadowsocks/clientconf.json
+nohup python3 ./shadowsocksr/shadowsocks/local.py -c ./shadowsocksr/shadowsocks/clientconf.json > /dev/null 2>&1 &
 echo "Client successfully ran in the background!"
 exit 0
